e22ed3e11138e9b03661db871a706c654983aaf3
Andres20000 <30440211+Andres20000@users.noreply.github.com>
2017-07-28 15:37:43 -0500

    Primer commit

---
+++ HolaGitHub/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  HolaGitHub
+//
+//  Created by Andres Garcia on 7/28/17.
+//  Copyright © 2017 Tactoapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func didTapBotonUno(_ sender: Any) {
+        print("boton1")
+        
+    }
+    
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'HolaGitHub' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for HolaGitHub
+  pod 'Firebase/Core'
+
+end
