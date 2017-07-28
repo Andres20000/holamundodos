1dd50ab1c41d12449a65e09b97b04f56276afd3c
Andres20000 <30440211+Andres20000@users.noreply.github.com>
2017-07-28 16:12:42 -0500

    subida de view

--- HolaGitHub/ViewController.swift
+++ HolaGitHub/ViewController.swift
@@ -21,7 +21,8 @@ class ViewController: UIViewController {
     }
 
     @IBAction func didTapBotonUno(_ sender: Any) {
-        print("boton1")
+        print("boton")
+        
         
     }
     
